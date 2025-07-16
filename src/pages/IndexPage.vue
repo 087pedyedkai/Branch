78971792842b8917745bbde65c0904430734df2d
087pedyedkai <omen007.1@gmail.com>
2025-07-16 10:51:40 +0700

    Age warning

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -21,7 +21,7 @@
         lazy-rules
         :rules="[
           val => val !== null && val !== '' || 'กรุณาพิมพ์อายุของคุณ',
-          val => val > 0 && val < 100 || 'กรุณาพิมพ์อายุที่ถูกต้อง (1-99)'
+          val => val > 0 && val < 100 || 'กรุณาพิมพ์อายุที่ถูกต้องนะจ๊ะ '
         ]"
       />
       <q-toggle v-model="accept" label="I accept the license and terms" />
